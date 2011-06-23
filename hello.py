0cd13e65fca199626657c50e028e6f7e44ea66de
Kurt Miebach <kwmiebach@gmail.com>
2011-06-23 20:14:36 +0200

    Switched to mako templates. Same functionality as before.

--- hello.py
+++ hello.py
@@ -2,16 +2,25 @@ from google.appengine.ext import db
 from vendor import web
 from models import *
 
+import os
+import sys
+sys.path.append("vendor")
+
+from vendor.mako.template import Template 
+from vendor.mako.lookup import  TemplateLookup 
+
 urls = (
   '/', 'index',
   '/list', 'list'
 )
 
-render = web.template.render('templates', base='base')
+
+tdir = os.path.join(os.path.dirname(__file__),"templates")
+tlook = TemplateLookup(directories=[tdir])
 
 class index:
     def GET(self):
-        return render.index()
+        return tlook.get_template("index.html").render()    
     def POST(self):
         i = web.input()
         person = Person()
@@ -21,8 +30,8 @@ class index:
 
 class list:
     def GET(self):
-        people = db.GqlQuery("SELECT * FROM Person ORDER BY created DESC LIMIT 10")
-        return render.list(people)
+      people = db.GqlQuery("SELECT * FROM Person ORDER BY created DESC LIMIT 10")
+      return tlook.get_template("list.html").render(people=people)    
 
 app = web.application(urls, globals())
 main = app.cgirun()
